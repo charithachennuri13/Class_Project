545786e035e55b1edaa53cabbf4e0e9cbb9f51b1
charitha chennuri <charithachennuri13@gmail.com>
2024-07-31 11:14:43 +0530

    Add files via upload

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,7 +1,7 @@
 /* GET homepage */
 const index = (req, res) => {
-    res.render('index', { title: 'Class Project' });
-    };
+    res.render('index', { title: 'Pharmacy Management System' });
+   };
 module.exports = {
 index
- };
+};
